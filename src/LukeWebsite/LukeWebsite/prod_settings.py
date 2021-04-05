c8ce56dd9ba75bd34cb66b24551626bfe03c6f94
Luke Peltier <lukepeltier@hey.com>
2021-04-04 22:25:29 -0500

    Add crispy production settings

--- src/LukeWebsite/LukeWebsite/prod_settings.py
+++ src/LukeWebsite/LukeWebsite/prod_settings.py
@@ -133,3 +133,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+#Custom
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
