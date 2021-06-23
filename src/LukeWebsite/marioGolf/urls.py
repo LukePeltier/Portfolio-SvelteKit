551902ca67fa79558069a4c9acd72fcc9e906a18
Luke Peltier <lukepeltier@hey.com>
2021-06-22 19:06:08 -0500

    Fix errors

--- src/LukeWebsite/marioGolf/urls.py
+++ src/LukeWebsite/marioGolf/urls.py
@@ -3,7 +3,7 @@ import marioGolf.views as views
 
 urlpatterns = [
     path('', views.base_views.Dashboard.as_view(), name='marioGolfHome'),
-    path('characters/', views.base_views.CharacterList.as_view(), name='characterListView'),
-    path('characters/<int:pk>/', views.base_views.CharacterDetailView.as_view(), name='detailCharacter'),
-    path('players/<int:pk>/', views.base_views.PlayerDetailView.as_view(), name='detailPlayer'),
+    path('characters/', views.base_views.CharacterList.as_view(), name='characterListViewMario'),
+    path('characters/<int:pk>/', views.base_views.CharacterDetailView.as_view(), name='detailCharacterMario'),
+    path('players/<int:pk>/', views.base_views.PlayerDetailView.as_view(), name='detailPlayerMario'),
 ]
