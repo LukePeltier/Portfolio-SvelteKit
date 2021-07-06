567228665023515d2796343e8f70b1ee3d3d2b67
Luke Peltier <lukepeltier@hey.com>
2021-07-06 13:28:56 -0500

    Increase testing coverage stat

--- src/LukeWebsite/homepagePortfolio/tests.py
+++ src/LukeWebsite/homepagePortfolio/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class BasicViewsTest(TestCase):
+    def test_dashboard_view(self):
+        url = reverse("home")
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
