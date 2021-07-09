9035788cbfba3616fa3bd87fd9323f7359f5f610
Luke Peltier <lukepeltier@hey.com>
2021-07-09 10:55:14 -0500

    Default to production environment

--- src/LukeWebsite/LukeWebsite/wsgi.py
+++ src/LukeWebsite/LukeWebsite/wsgi.py
@@ -17,6 +17,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LukeWebsite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LukeWebsite.settings.prod')
 
 application = get_wsgi_application()
