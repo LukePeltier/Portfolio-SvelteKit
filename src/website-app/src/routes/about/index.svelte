da01dde3b2a223ddee73cdc65ba1a986801bbceb
Luke Peltier <luke@lukepeltier.com>
2022-08-07 14:21:58 -0500

    Clean up about page

--- src/website-app/src/routes/about/index.svelte
+++ src/website-app/src/routes/about/index.svelte
@@ -4,20 +4,23 @@
 <script lang="ts">
 </script>
 
-<h1 class="font-robotomono font-semibold text-5xl text-center text-zinc-300  mb-20">About Page</h1>
-<div class="flex flex-col justify-center font-robotomono text-xl text-center text-zinc-300 basis-6/12">
-    <p>Hi, I'm a software engineer who uses</p>
-    <div class="max-w-fit border-solid rounded-lg border-2 mt-4 mb-4 grid grid-cols-3 gap-4 justify-items-center basis-1/12 self-center p-2 border-sky-500">
-        <p class="">C++</p>
-        <p>Python</p>
-        <p>Java</p>
-        <p class="">PHP</p>
-        <p>Javascript</p>
-        <p>MySQL</p>
-        <p class= "col-span-3">Bash</p>
+<div class="flex flex-col">
+    <h1 class="font-robotomono font-semibold text-5xl text-center text-zinc-300 mb-20">About Page</h1>
+    <div class="flex flex-col self-center font-robotomono text-xl text-center text-zinc-300 max-w-prose">
+        <p>Hi, I'm a software engineer who uses</p>
+        <div class="max-w-fit border-solid rounded-lg border-2 mt-4 mb-4 grid grid-cols-3 gap-4 justify-items-center self-center p-2 border-sky-500">
+            <p class="">C++</p>
+            <p>Python</p>
+            <p>Java</p>
+            <p class="">PHP</p>
+            <p>Javascript</p>
+            <p>MySQL</p>
+            <p class= "col-span-3">Bash</p>
+        </div>
+        <p>
+        This site is mainly just for hosting my personal projects. Checkout my
+        <a class="underline decoration-sky-500 hover:text-white" href="https://github.com/LukePeltier/">Github</a>
+        </p>
+        <p class="p-10">*I use many other languages and technologies that I did not list here, but I would describe these as my most currently used. Email me at <a class="underline decoration-sky-500 hover:text-white" href="mailto:contact@lukepeltier.com">contact@lukepeltier.com</a> with any questions.</p>
     </div>
-    <p>
-    This site is mainly just for hosting my personal projects. Checkout my
-    <a class="underline decoration-sky-500 hover:text-white" href="https://github.com/LukePeltier/">Github</a>
-    </p>
 </div>
