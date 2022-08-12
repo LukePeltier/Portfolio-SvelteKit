1f35dd89b21c2248b3f349004810127bf0b86434
Luke Peltier <luke@lukepeltier.com>
2022-08-12 14:12:21 -0500

    Format with prettier

--- src/website-app/src/lib/HomeNavbar.svelte
+++ src/website-app/src/lib/HomeNavbar.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
-    let navItems = [
-    {title: "Home", url: "/"},
-    {title: "About", url: "/about"},
-  ]
+  let navItems = [
+    { title: 'Home', url: '/' },
+    { title: 'About', url: '/about' }
+  ];
 </script>
 
-
 <nav class="flex sm:justify-center space-x-4 bg-zinc-800 mb-8">
-    {#each navItems as nav}
-        <a href={nav.url} class="rounded-lg px-3 py-2 text-zinc-300 font-medium hover:bg-zinc-900 hover:text-zinc-100">{nav.title}</a>
-    {/each}
+  {#each navItems as nav}
+    <a
+      href={nav.url}
+      class="rounded-lg px-3 py-2 text-zinc-300 font-medium hover:bg-zinc-900 hover:text-zinc-100"
+      >{nav.title}</a
+    >
+  {/each}
 </nav>
