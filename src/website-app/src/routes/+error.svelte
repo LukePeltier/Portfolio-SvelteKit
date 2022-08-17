4640fe55f3dc516d0430810c9d21215b615b50a6
Luke Peltier <luke@lukepeltier.com>
2022-08-16 20:44:57 -0500

    svelte-migrate: update files

--- src/website-app/src/routes/+error.svelte
+++ src/website-app/src/routes/+error.svelte
@@ -1,26 +1,7 @@
-<script context="module">
-  throw new Error("@migration task: Replace error load function (https://github.com/sveltejs/kit/discussions/5774#discussioncomment-3293209)");
-
-  // /** @type {import('@sveltejs/kit').Load} */
-  // export function load({ error, status }) {
-  //   if (error == null) {
-  //     throw new Error(`No error found`);
-  //   }
-  //   return {
-  //     props: {
-  //       title: `${status}: ${error.message}`
-  //     }
-  //   };
-  // }
-</script>
-
 <script>
-  /**
-   * @type {string | string[]}
-   */
-  export let title;
+  import { page } from '$app/stores';
 </script>
 
-<h1 class="text-5xl text-center">{title}</h1>
+<h1 class="text-5xl text-center">{$page.status} : {$page.error.message}</h1>
 
 <p>Oopsie woopsie</p>
