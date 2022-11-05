52a48b3e5456be6e83ced37527f324f23b14d01f
Luke Peltier <luke@lukepeltier.com>
2022-11-05 14:12:53 -0500

    Use docker

--- src/website-app/playwright.config.ts
+++ src/website-app/playwright.config.ts
@@ -1,10 +1,10 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
-	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173
-	}
+  webServer: {
+    command: 'yarn run build && yarn run preview',
+    port: 4173
+  }
 };
 
 export default config;
--- src/website-app/src/routes/(home)/+layout.svelte
+++ src/website-app/src/routes/(home)/+layout.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import '../../app.css';
   import Navbar from '$lib/HomeNavbar.svelte';
 </script>
 
